73da82f60b5762dfa60e5178cfbd426a9618547b
Jose Pujol <josepujol21@gmail.com>
2023-07-06 20:34:34 -0500

    initialize repo

---
+++ login_auth_tutorial/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  login_auth_tutorial
+//
+//  Created by Jose  Pujol  on 7/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color.black
+            
+            RoundedRectangle(cornerRadius: 30, style: .continuous)
+                .foregroundStyle(.linearGradient(colors: [.pink, .red], startPoint: .topLeading, endPoint: .bottomTrailing))
+                .frame(width: 1000, height: 400)
+                .rotationEffect(.degrees(135))
+                .offset(y: -350)
+        }
+        .ignoresSafeArea()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ login_auth_tutorial/login_auth_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  login_auth_tutorialApp.swift
+//  login_auth_tutorial
+//
+//  Created by Jose  Pujol  on 7/6/23.
+//
+
+import SwiftUI
+
+@main
+struct login_auth_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
